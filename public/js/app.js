8214b47d4f0b62c07254dbeccb6925c312250448
Nadav <nadavshtibel@gmail.com>
2022-09-10 19:24:00 +0300

    setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -6,11 +6,12 @@ const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 
 
+
  weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
     const location = search.value
 
-fetch('http://localhost:3000/weather?address=' + location)
+fetch('/weather?address=' + location)
  .then(res => res.json()
  .then(data => {
     if (data.error) console.log(data.error);
